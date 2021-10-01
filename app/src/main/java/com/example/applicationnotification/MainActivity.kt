ac8f2c2a71c86de71d0f943a7b4650e53f0ae8b9
AndWork <anderson.work.00@gmail.com>
2021-10-01 14:58:50 -0300

    Repo App Notification

---
+++ app/src/main/java/com/example/applicationnotification/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.applicationnotification
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import com.google.firebase.messaging.FirebaseMessaging
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var btnSend: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnSend = findViewById(R.id.button_send_notification)
+        btnSend.setOnClickListener{
+            this.showNotification("1234", "bootcamp Android", "Kotlin Android curso")
+        }
+
+        Log.i("**newToken", FirebaseMessaging.getInstance().token.toString())
+    }
+}
